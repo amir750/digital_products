3d48cb0efd2eed1b3951e56da4263ea74ccdc392
amir16v <amirmathmatic12@gmail.com>
2023-09-17 21:27:07 +0330

    Products model added.

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class Category(models.Model):
     title=models.CharField(_('title'),max_length=55)
     description=models.TextField(_(' description'),blank=True)
-    avatar=models.ImageField(_('avatar'),blank=True)
+    avatar=models.ImageField(_('avatar'),blank=True,upload_to='categories/')
     is_enable=models.BooleanField(_('is_enable'),default=True)
     created_time=models.DateTimeField(_('created_time'),auto_now_add=True)
     updated_time=models.DateTimeField(_(' updated_time'),auto_now=True)
@@ -17,8 +17,26 @@ class Category(models.Model):
 class Product(models.Model):
     title=models.CharField(_('title'),max_length=55)
     description=models.TextField(_(' description'),blank=True)
-    avatar=models.ImageField(_('avatar'),blank=True)
+    avatar=models.ImageField(_('avatar'),blank=True,upload_to='products/')
     is_enable=models.BooleanField(_('is_enable'),default=True)
     categories=models.ManyToManyField('Category',verbose_name=_('categories'),blank=True)
     created_time = models.DateTimeField(_('created_time'), auto_now_add=True)
     updated_time = models.DateTimeField(_(' updated_time'), auto_now=True)
+
+    class Meta:
+        db_table='products'
+        verbose_name=_('product')
+        verbose_name_plural = _('products')
+
+    class File:
+        product=models.ForeignKey('Product',verbose_name=_('product'),on_delete=models.CASCADE)
+        title = models.CharField(_('title'), max_length=55)
+        file=models.FileField(_('file'),upload_to='files/%Y/%m/%d/')
+        is_enable = models.BooleanField(_('is_enable'), default=True)
+        created_time = models.DateTimeField(_('created_time'), auto_now_add=True)
+        updated_time = models.DateTimeField(_(' updated_time'), auto_now=True)
+
+        class Meta:
+            db_table = 'files'
+            verbose_name = _('file')
+            verbose_name_plural = _('files')
