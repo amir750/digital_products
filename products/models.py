be24399571f5efc52f42a597ad660b3598094ee2
amir16v <amirmathmatic12@gmail.com>
2023-09-19 17:56:36 +0330

    Products model added.

--- products/models.py
+++ products/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-import django
+import django.db.models.deletion
 # Create your models here.
 class Category(models.Model):
-    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True,
-                               on_delete=django.db.models.deletion.CASCADE)
+    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True,on_delete=django.db.models.deletion.CASCADE)
     title=models.CharField(_('title'),max_length=55)
     description=models.TextField(_(' description'),blank=True)
     avatar=models.ImageField(_('avatar'),blank=True,upload_to='categories/')
     is_enable=models.BooleanField(_('is_enable'),default=True)
     created_time=models.DateTimeField(_('created_time'),auto_now_add=True)
     updated_time=models.DateTimeField(_(' updated_time'),auto_now=True)
-    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True,
-                               on_delete=django.db.models.deletion.CASCADE)
+
     class Meta:
         db_table='categories'
         verbose_name='category'
@@ -26,6 +24,7 @@ class Product(models.Model):
     categories=models.ManyToManyField('Category',verbose_name=_('categories'),blank=True)
     created_time = models.DateTimeField(_('created_time'), auto_now_add=True)
     updated_time = models.DateTimeField(_(' updated_time'), auto_now=True)
+
 
 
     class Meta:
